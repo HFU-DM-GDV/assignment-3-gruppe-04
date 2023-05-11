42a21927edc2f40b378825b9946799526595f8c9
Keros918 <haasleon.s@gmx.de>
2023-05-11 12:53:18 +0200

    firts test

--- hybrid-imaging.py
+++ hybrid-imaging.py
@@ -2,13 +2,25 @@ import cv2
 import numpy as np
 
 def low_pass_filter(img):
-    print()
+    kernel_size = 33
+    sigma = -1
+    kernel = cv2.getGaussianKernel(kernel_size, sigma)
+    kernel = np.transpose(kernel) * kernel
+    print('Kernel: ', kernel)
+    return cv2.filter2D(img, -1, kernel)
 
 def high_pass_filter(img):
-    print()
+    kernel_size = 33
+    sigma = -1
+    kernel = cv2.getGaussianKernel(kernel_size, sigma)
+    kernel = np.transpose(kernel) * kernel
+    i_kernel = (1 - kernel)
+    i_kernel = i_kernel / i_kernel.sum()
+    print('Inverse Kernel: ', i_kernel)
+    return cv2.filter2D(img, -1, i_kernel)
 
 def merge_images(img_1, img_2):
-    print()
+    return cv2.addWeighted(img_1, 0.5, img_2, 0.5, 0)
 
 def main():
     img_1 = cv2.imread('data/images/angry_man.png')
@@ -20,8 +32,11 @@ def main():
     hybrid_image = merge_images(low_f_img, high_f_img)
 
     title = 'Hybrid image'
-    cv2.namedWindow(title)
+    cv2.namedWindow(title, cv2.WINDOW_GUI_EXPANDED)
     cv2.imshow(title, hybrid_image)
+
+    if cv2.waitKey(0) == ord('q'):
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
