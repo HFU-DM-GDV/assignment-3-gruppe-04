860b299dc23b7e0ac4390fa20a3fddd702ae185d
Keros918 <haasleon.s@gmx.de>
2023-05-11 11:50:41 +0200

    better script skellet

--- hybrid-imaging.py
+++ hybrid-imaging.py
@@ -7,6 +7,9 @@ def low_pass_filter(img):
 def high_pass_filter(img):
     print()
 
+def merge_images(img_1, img_2):
+    print()
+
 def main():
     img_1 = cv2.imread('data/images/angry_man.png')
     img_2 = cv2.imread('data/images/woman.png')
@@ -14,7 +17,11 @@ def main():
     low_f_img = low_pass_filter(img_1)
     high_f_img = high_pass_filter(img_2)
 
-    
+    hybrid_image = merge_images(low_f_img, high_f_img)
+
+    title = 'Hybrid image'
+    cv2.namedWindow(title)
+    cv2.imshow(title, hybrid_image)
 
 if __name__ == '__main__':
     main()
